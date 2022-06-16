4284959433a6f9d6e57f3f1d95f5d2f6fe777fc3
Zemledelec <mgevlich@gmail.com>
2022-06-16 14:39:52 +0400

    fixed

--- exec.sh
+++ exec.sh
@@ -1,7 +1,7 @@
 if [[ $5 -eq "norgbify" ]]
 then
-    /bin/bash ./warprgbify.sh $3
-else
     /bin/bash ./warp.sh $3
+else
+    /bin/bash ./warprgbify.sh $3
 fi
 /bin/bash ./geoTiff2Tiles.sh $1 $2 $3 $4
